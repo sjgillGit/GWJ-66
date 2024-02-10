1c10e4a3d1f70e6cbb44623f8c2bc9c9b7cf773a
mykhailo <mikhail.gavrilec@gmail.com>
2024-02-10 17:32:19 +0200

    add update to main scene

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,12 +2,12 @@ extends Node
 
 
 @export var player_scene:PackedScene
-@export var world_scene:PackedScene
+@export var world_scenes:Array[PackedScene]
 var current_world:Node3D
 
 # game began
 func _ready():
-	create_world()
+	change_world(world_scenes[0])
 	_connect_world_signals()
 	setup_world([])
 
@@ -16,10 +16,16 @@ func _connect_world_signals():
 	pass
 
 
-func create_world():
-	current_world = world_scene.instantiate()
+func create_world(new_world:PackedScene):
+	current_world = new_world.instantiate()
 	$World.add_child(current_world)
 
 
 func setup_world(world_objects:Array):
 	current_world.populate_world(world_objects)
+
+
+func change_world(new_world:PackedScene):
+	NodeTools.delete_all_children($World)
+	create_world(new_world)
+	
