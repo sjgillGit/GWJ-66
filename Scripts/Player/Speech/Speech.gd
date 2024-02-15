eacb105b59763652fe457a0eee98a4b86763f148
mykhailo <mikhail.gavrilec@gmail.com>
2024-02-15 21:26:26 +0200

    add ability to make speech continue to next text only with left click, also add ability to speak out faster

--- Scripts/Player/Speech/Speech.gd
+++ Scripts/Player/Speech/Speech.gd
@@ -3,19 +3,29 @@ extends Node3D
 
 @export var dialogue:Dialogue
 var speaker_name:String
+var skip_pressed:bool = false
 
 
 func _ready():
 	dialogue.speech_triggered.connect(_on_speech_triggered)
 
 
+func _input(event):
+	if event.is_action_pressed("ui_mouse_left"):
+		skip_pressed = true
+
+
 func _write_speech(text:String):
+	skip_pressed = false
 	get_tree().paused = true
 	$Text.text = ""
 	for char in text:
-		await get_tree().create_timer(0.1).timeout
+		if !skip_pressed:
+			await get_tree().create_timer(0.1).timeout
 		$Text.text += char
-	await get_tree().create_timer(0.5).timeout
+	skip_pressed = false
+	while !skip_pressed:
+		await get_tree().create_timer(0.1).timeout
 	$Text.text = ""
 	dialogue.receive_response()
 	get_tree().paused = false
