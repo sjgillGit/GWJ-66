3712aa16d87c93532d33011cf50f4f05b3713ec4
KalebClint <kalebclintmail@gmail.com>
2024-02-17 07:35:04 +1300

    Merge items and Servant Quarters

--- Scripts/Inventory/InventorySpaceScript.gd
+++ Scripts/Inventory/InventorySpaceScript.gd
@@ -4,19 +4,30 @@ extends Panel
 ## Put the name of the item meant to go in this inventory space.
 @export var inventoryTag = ""
 
+@export var itemImageTexutre = Texture
+
 ## Check this if its put in the quick inventory
 @export var quickInventorySlot = false
 
-@export var itemImageTexutre = Texture
-@export var itemSilhouetteTexture = Texture
+
+#Merging Items
+@export var mergeItemSlot = false
+var mouseOverMergeSlot = false
+var merged = 0
+
+
+var normalSlot = true
+var mouseOverNormalSlot = false
+
 
 # These variables are the only thing needed to be saved for inventory.
 var hasItemBool = false
 var itemUsedBool = false
 
 #Player Dragging And Dropping
-var mouseOverItem = false
+var mouseOverQuickSlot = false
 var dragging = false
+
 
 #Sprite for the item, will be changed between - ? - Item Image - Silhouette Of Item -
 @onready var itemSprite = $ItemSprite/Item
@@ -30,30 +41,66 @@ var dragging = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
+	if !quickInventorySlot && !mergeItemSlot:
+		normalSlot = true
+	else:
+		normalSlot = false
+	
 	# Here Load Save File if any and change image #
 	
-	#Change text if not quick, based on whether known or not
-	if !quickInventorySlot :
-		
+	
+	#Change text if normal, based on whether known or not
+	if normalSlot :
 		if hasItemBool || itemUsedBool:
 			itemName.text = "[center]"+inventoryTag+"[/center]"
 		else:
 			itemName.text = "[center]"+"???"+"[/center]"
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	#Implement dragging, if true follow cursor. Otherwise move back.
-	if dragging == true:
+	if dragging:
 		itemSprite.global_position = get_global_mouse_position()
+		
+		if normalSlot:
+			#If dragging a normal slot item, set the variables to parent script to be access by merge slot.
+			Inventory.mergingItemSprite = itemImageTexutre
+			Inventory.mergingItemTag = inventoryTag
+			Inventory.dragging = true
 	else:
 		itemSprite.global_position = spritePos.global_position
 		
 	#If mouse over and left button held down, as well as having the item. Start dragging, otherwise stop.
-	if hasItemBool == true:
+	checkDragging()
+	
+	# Basically, if mouse is hovering over merge while dragging an item, and they let go, it moves it.
+	if mouseOverMergeSlot == true && Inventory.dragging:
+		if Input.is_action_just_released("ui_mouse_left"):
+			#Set variables of the merge slot to match dragged item
+			inventoryTag = Inventory.mergingItemTag
+			itemImageTexutre = Inventory.mergingItemSprite
+			itemSprite.texture = itemImageTexutre
+			Inventory.hideError()
+
+func checkDragging():
+	if hasItemBool:
 		if Input.is_action_just_pressed("ui_mouse_left"):
-			if mouseOverItem == true:
+			if mouseOverQuickSlot || mouseOverNormalSlot:
 				dragging = true
+				if !quickInventorySlot:
+					Inventory.dragging = true
+			elif !quickInventorySlot:
+				Inventory.dragging = false
 		elif Input.is_action_just_released("ui_mouse_left"):
 			dragging = false
+
+func itemUsed(itemTag):
+	#check if item picked up should be put here.
+	if itemTag == inventoryTag && hasItemBool == true:
+		hasItemBool = false
+		itemUsedBool = true
+		#Change image to - Item Silhouette
+		itemSprite.modulate.a = 0.3
+		itemSprite.texture = itemImageTexutre
 
 func itemGrabbed(itemTag):
 	#check if item picked up should be put here.
@@ -61,32 +108,40 @@ func itemGrabbed(itemTag):
 		hasItemBool = true
 		#Change image to - Item Image And change text if normal
 		itemSprite.texture = itemImageTexutre
-		
-		if !quickInventorySlot:
+		if normalSlot:
 			itemName.text = "[center]"+inventoryTag+"[/center]"
-		
-func itemUsed(itemTag):
-	#check if item picked up should be put here.
-	if itemTag == inventoryTag && hasItemBool == true:
-		hasItemBool = false
-		itemUsedBool = true
-		#Change image to - Item Silhouette
-		itemSprite.modulate.a = 0.5
-		itemSprite.texture = itemSilhouetteTexture
 
 func _on_mouse_entered():
 	#Only allow dragging if in quick inventory
 	if quickInventorySlot:
-		mouseOverItem = true
+		mouseOverQuickSlot = true
+		
 	# Do this only if its a normal inventory slot
-	if !quickInventorySlot:
+	elif mergeItemSlot:
+		mouseOverMergeSlot = true
+	
+	elif normalSlot:
+		mouseOverNormalSlot = true
 		Inventory.showDescription(inventoryTag,hasItemBool,itemUsedBool)
 	
 func _on_mouse_exited():
 	#Only allow dragging if in quick inventory
 	if quickInventorySlot:
-		mouseOverItem = false
+		mouseOverQuickSlot = false
+		
 	# Do this only if its a normal inventory slot
-	if !quickInventorySlot:
+	elif mergeItemSlot:
+		mouseOverMergeSlot = false
+		
+	elif normalSlot:
+		mouseOverNormalSlot = false
 		Inventory.hideDescription()
 
+func sendMerges():
+	# Calls merged slot groups, they then send tag to parent script.
+	Inventory.mergeItems(inventoryTag)
+
+func clear():
+	inventoryTag = ""
+	itemImageTexutre = null
+	itemSprite.texture = null
--- Scripts/Objects/Pickable.gd
+++ Scripts/Objects/Pickable.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
+@export var tag = ""
 
 signal picked(obj_name:String)
 
 
 func _on_area_body_entered(body):
 	picked.emit(name)
+	get_tree().call_group("InventorySpot", "itemGrabbed", tag)
