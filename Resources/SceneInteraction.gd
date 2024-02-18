1dcb383bbfec5763c961db126591b8675b37d411
mykhailo <mikhail.gavrilec@gmail.com>
2024-02-18 17:18:58 +0200

    add interaction work in progress

---
+++ Resources/SceneInteraction.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name SceneInteraction
+
+
+var current_raycasted_object:Area3D
--- Scripts/Player/CameraController.gd
+++ Scripts/Player/CameraController.gd
@@ -8,6 +8,7 @@ extends Camera3D
 class_name CameraController
 
 @export var player: Player : set = set_player
+@export var scene_interaction:SceneInteraction
 var player_movement: MovementController
 var is_moving: bool
 
@@ -27,15 +28,21 @@ func _process(_delta) -> void:
 		var new_target = get_mouse_world_position()
 		if new_target != Vector3.ZERO:
 			player_movement.set_target(new_target)
+	get_mouse_world_position(true)
 
 
-func get_mouse_world_position() -> Vector3:
+func get_mouse_world_position(is_interaction:bool=false) -> Vector3:
 	var state = get_world_3d().direct_space_state
 	var mouse_pos = get_viewport().get_mouse_position()
 	var ray_origin = project_ray_origin(mouse_pos)
 	var ray_end = ray_origin + project_ray_normal(mouse_pos) * RAY_LENGTH
 	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	query.collide_with_areas = is_interaction
 	var result = state.intersect_ray(query)
 	if result && result.has("position"):
 		return result.position
+	if result.has("collider"):
+		if result.collider is Area3D and is_interaction:
+			scene_interaction.current_raycasted_object = result.collider
+			print(result.collider)
 	return Vector3.ZERO
